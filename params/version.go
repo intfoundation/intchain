4d156a20c1e50cdeea5acc850effc8636097c9a2
like <likeaixi@gmail.com>
2020-05-12 16:12:27 +0800

    cmd/intchain: add version cmd

--- params/version.go
+++ params/version.go
@@ -23,7 +23,7 @@ import (
 const (
 	VersionMajor = 4          // Major version component of the current release
 	VersionMinor = 0          // Minor version component of the current release
-	VersionPatch = 02         // Patch version component of the current release
+	VersionPatch = 2          // Patch version component of the current release
 	VersionMeta  = "unstable" // Version metadata to append to the version string
 )
 
