4ba551fb3abf001e4572ccd36310e43830200379
like <likeaixi@gmail.com>
2020-08-10 14:15:58 +0800

    consensus/ipbft/epoch: modified log

--- consensus/ipbft/epoch/epoch.go
+++ consensus/ipbft/epoch/epoch.go
@@ -29,7 +29,7 @@ const (
 	EPOCH_SAVED                     // value --> 3
 
 	MinimumValidatorsSize = 13
-	MaximumValidatorsSize = 52 // TODO the max validator size will increate to 100 in the future
+	MaximumValidatorsSize = 52 // TODO the max validator size will increase to 100 in the future
 
 	epochKey       = "Epoch:%v"
 	latestEpochKey = "LatestEpoch"
