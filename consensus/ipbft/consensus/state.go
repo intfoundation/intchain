ddfdffca1ed9f0f43c3ae120771f9d47dd7e2a50
like <likeaixi@gmail.com>
2020-11-30 10:35:52 +0800

    intchain/consensus/ipbft/consensus: add log for default set proposal

--- consensus/ipbft/consensus/state.go
+++ consensus/ipbft/consensus/state.go
@@ -1585,12 +1585,25 @@ func (cs *ConsensusState) defaultSetProposal(proposal *types.Proposal) error {
 	}
 
 	if proposal.Round == cs.Round {
+		fmt.Printf("default set proposal1, proposal.Round %v, cs.Round %v\n", proposal.Round, cs.Round)
+		fmt.Printf("default set proposal1, proposal %v, cs %v\n", proposal, cs)
+		fmt.Printf("default set proposal1, proposal %v\n", proposal.Signature)
+		fmt.Printf("default set proposal1, cs.GetProposer().PubKey %v\n", cs.GetProposer().PubKey)
+		fmt.Printf("default set proposal1, cs.chainConfig.IntChainId %v\n", cs.chainConfig.IntChainId)
+		fmt.Printf("default set proposal1, verifyBytes(cs, proposal.Signature) %v, %v\n", types.SignBytes(cs.chainConfig.IntChainId, proposal), proposal.Signature)
+
 		// Verify signature
 		if !cs.GetProposer().PubKey.VerifyBytes(types.SignBytes(cs.chainConfig.IntChainId, proposal), proposal.Signature) {
 			return ErrInvalidProposalSignature
 		}
 
 	} else /*proposal.Round < cs.Round*/ {
+		fmt.Printf("default set proposal2, proposal.Round %v, cs.Round %v\n", proposal.Round, cs.Round)
+		fmt.Printf("default set proposal2, proposal %v, cs %v\n", proposal, cs)
+		fmt.Printf("default set proposal2, proposal %v\n", proposal.Signature)
+		fmt.Printf("default set proposal2, cs.proposerByRound(proposal.Round).Proposer.PubKey %v\n", cs.proposerByRound(proposal.Round).Proposer.PubKey)
+		fmt.Printf("default set proposal2, cs.chainConfig.IntChainId %v\n", cs.chainConfig.IntChainId)
+		fmt.Printf("default set proposal2, verifyBytes(cs, proposal.Signature) %v, %v\n", types.SignBytes(cs.chainConfig.IntChainId, proposal), proposal.Signature)
 
 		// Verify signature
 		if !cs.proposerByRound(proposal.Round).Proposer.PubKey.VerifyBytes(types.SignBytes(cs.chainConfig.IntChainId, proposal), proposal.Signature) {
