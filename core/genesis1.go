4822ce372863795b9dd4a147dd45170d6d78b754
like <likeaixi@gmail.com>
2021-07-13 09:58:05 +0800

    rebuild test net genesis

--- core/genesis1.go
+++ core/genesis1.go
@@ -227,14 +227,14 @@ var DefaultTestnetGenesisJSON = `{
 		}
 	},
 	"nonce": "0x0",
-	"timestamp": "0x60e40779",
+	"timestamp": "0x60ecf254",
 	"extraData": "0x",
 	"gasLimit": "0x5f5e100",
 	"difficulty": "0x1",
 	"mixHash": "0x0000000000000000000000000000000000000000000000000000000000000000",
 	"coinbase": "0x0000000000000000000000000000000000000000",
 	"alloc": {
-		"71776d0f0a75cf2f05b35cc3308e265985618323": {
+		"9fe77cb625d2aa7a974b4ee88c9095893da8c1a0": {
 			"balance": "0x29569e2db20e16b46000000",
 			"amount": "0x54b40b1f852bda000000"
 		}
