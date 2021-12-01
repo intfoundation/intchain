120d679b3889096c3afd986287632e26eb0642f7
like <likeaixi@gmail.com>
2021-12-01 11:11:36 +0800

    update debug transaction

--- core/state_transition.go
+++ core/state_transition.go
@@ -498,7 +498,7 @@ func (st *StateTransition) TransitionDbTracer() (*ExecutionResult, *big.Int, err
 		return &ExecutionResult{
 			UsedGas:    st.gasUsed(),
 			Err:        nil,
-			ReturnData: nil,
+			ReturnData: []byte{1},
 		}, usedMoney, nil
 
 	}
