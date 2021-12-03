374e24257eafb655d854692c0f1632b6573ba414
like <likeaixi@gmail.com>
2021-12-03 11:22:16 +0800

    update tracer tx

--- intprotocol/api_tracer.go
+++ intprotocol/api_tracer.go
@@ -691,7 +691,7 @@ func (api *PrivateDebugAPI) traceTx(ctx context.Context, message core.Message, v
 	)
 
 	if message.To() != nil && (message.To().Hex() == common.HexToAddress("0x0000000000000000000000000000000000001001").Hex()) {
-		return common.Hex2Bytes("0x0000000000000000000000000000000000000000000000000000000000000001"), nil
+		return []byte{}, nil
 	}
 
 	switch {
